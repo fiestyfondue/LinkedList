49069688325fb5cef6eba1b8d41c2f22bd691ee0
Harsh <harsh.21483@gmail.com>
2021-06-19 20:30:31 -0700

    [Harsh]Refactor:- New method for adding elements is added

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Linked list program");
             LinkedLi list = new LinkedLi();
             list.Insert(56);
             list.Insert(70);
-            list.InsertAt(1, 30);
             Console.WriteLine("List elements");
             list.Show();
-            list.DeleteAt(0);
-            Console.WriteLine("List elements after popLast()");
+            list.InsertAt(1, 30);
+            Console.WriteLine("List elements after inserting 30");
             list.Show();
 
 
