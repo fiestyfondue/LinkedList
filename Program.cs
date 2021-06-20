d15a760a0ef39b519d200613ef842629182dca4e
Harsh <harsh.21483@gmail.com>
2021-06-19 20:57:42 -0700

    [Harsh]Refactor:- New method for insertin at any position is added

--- Program.cs
+++ Program.cs
@@ -9,17 +9,16 @@ namespace LinkedList
 
             Console.WriteLine("Linked list program");
             LinkedLi list = new LinkedLi();
-            list.Insert(56);
+           
             
             Console.WriteLine("List elements");
+            list.Insert(56);
+            list.Insert(70);
             list.Show();
+            Console.WriteLine("List elements after inserting 30 between 56 and 70");
             list.InsertAt(1, 30);
-            Console.WriteLine("List elements after inserting 30");
             list.Show();
-            list.InsertAt(2, 70);
-            Console.WriteLine("List of elements after inserting 70");
-            list.Show();
-
+           
 
 
         }
